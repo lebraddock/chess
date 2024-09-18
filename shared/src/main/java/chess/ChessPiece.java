0933ad0b028706a5a81bc262f18a9b6d1fbfc9c8
Cameron Braddock <cameron.braddock21@gmail.com>
2024-09-18 14:15:06 -0600

    Added pawn and knight moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Vector;
 
+import static chess.ChessPiece.PieceType.QUEEN;
+
 /**
  * Represents a single chess piece
  * <p>
@@ -198,6 +200,107 @@ public class ChessPiece {
 
                 }
                 break;
+            case KNIGHT:
+                posX = myPosition.getRow();
+                posY = myPosition.getColumn();
+                posMoves = new ArrayList<int[]>();
+                posMoves.add(new int[] {posX + 1,posY+2});
+                posMoves.add(new int[] {posX+2,posY+1});
+                posMoves.add(new int[] {posX+2,posY-1});
+                posMoves.add(new int[] {posX+1,posY-2});
+                posMoves.add(new int[] {posX-1,posY-2});
+                posMoves.add(new int[] {posX-2,posY-1});
+                posMoves.add(new int[] {posX-2,posY+1});
+                posMoves.add(new int[] {posX-1,posY+2});
+                for(int i = 0; i < 8; i++){
+                    posX = posMoves.get(i)[0];
+                    posY = posMoves.get(i)[1];
+                    if(!(posX > 8 || posX < 1 || posY > 8 || posY < 1)){
+                        if(board.getPiece(new ChessPosition(posX,posY)) == null){
+                            moves.add(new ChessMove(myPosition, new ChessPosition(posX,posY), null));
+                            //System.out.println(posX + ", " + posY);
+                        }
+                        else if(board.getPiece(new ChessPosition(posX,posY)).getTeamColor() != color){
+                            // if we run into opponents pice
+                            moves.add(new ChessMove(myPosition, new ChessPosition(posX,posY), null));
+                            //System.out.println(posX + ", " + posY);
+                        }
+                    }
+
+                }
+                break;
+            case PAWN:
+                posMoves = new ArrayList<int[]>();
+                posY = myPosition.getRow();
+                posX = myPosition.getColumn();
+                switch (color) {
+                    case WHITE:
+
+                        //move forward 1
+                        if (board.getPiece(new ChessPosition(posY + 1, posX)) == null) {
+                            posMoves.add(new int[]{posY + 1, posX});
+                        }
+                        //move forward 2
+                        if(posY == 2){
+                            if (board.getPiece(new ChessPosition(posY+2, posX)) == null && board.getPiece(new ChessPosition(posY+1, posX)) == null) {
+                                posMoves.add(new int[]{posY + 2, posX});
+                            }
+                        }
+                        //capture left
+                        if (board.getPiece(new ChessPosition(posY + 1, posX + 1)) != null) {
+                            if (board.getPiece(new ChessPosition(posY + 1, posX + 1)).getTeamColor() != color) {
+                                posMoves.add(new int[]{posY + 1, posX + 1});
+                            }
+                        }
+                        //capture right
+                        if (board.getPiece(new ChessPosition(posY + 1, posX - 1)) != null) {
+                            if (board.getPiece(new ChessPosition(posY + 1, posX - 1)).getTeamColor() != color) {
+                                posMoves.add(new int[]{posY + 1, posX - 1});
+                            }
+                        }
+
+
+                        break;
+                    case BLACK:
+                        //move forward 1
+                        if (board.getPiece(new ChessPosition(posY - 1, posX)) == null) {
+                            posMoves.add(new int[]{posY - 1, posX});
+                        }
+                        //move forward 2
+                        if(posY==7){
+                            if (board.getPiece(new ChessPosition(posY-2, posX)) == null && board.getPiece(new ChessPosition(posY-1, posX)) == null) {
+                                posMoves.add(new int[]{posY - 2, posX});
+                            }
+                        }
+                        //capture left
+                        if (board.getPiece(new ChessPosition(posY - 1, posX + 1)) != null) {
+                            if (board.getPiece(new ChessPosition(posY - 1, posX + 1)).getTeamColor() != color) {
+                                posMoves.add(new int[]{posY - 1, posX + 1});
+                            }
+                        }
+                        //capture right
+                        if (board.getPiece(new ChessPosition(posY - 1, posX - 1)) != null) {
+                            if (board.getPiece(new ChessPosition(posY - 1, posX - 1)).getTeamColor() != color) {
+                                posMoves.add(new int[]{posY - 1, posX - 1});
+                            }
+                        }
+                        break;
+                }
+                for(int i = 0; i < posMoves.size(); i++) {
+                    posX = posMoves.get(i)[1];
+                    posY = posMoves.get(i)[0];
+                    if (posY == 8 || posY == 1) {
+                        moves.add(new ChessMove(myPosition, new ChessPosition(posY, posX), PieceType.QUEEN));
+                        moves.add(new ChessMove(myPosition, new ChessPosition(posY, posX), PieceType.ROOK));
+                        moves.add(new ChessMove(myPosition, new ChessPosition(posY, posX), PieceType.BISHOP));
+                        moves.add(new ChessMove(myPosition, new ChessPosition(posY, posX), PieceType.KNIGHT));
+                    } else {
+                        moves.add(new ChessMove(myPosition, new ChessPosition(posY, posX), null));
+                    }
+                }
+                break;
+
+
         }
 
 
