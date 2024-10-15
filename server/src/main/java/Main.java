b904e49b300b4cb041919084f1280b8e626f9342
Cameron Braddock <cameron.braddock21@gmail.com>
2024-10-15 16:24:33 -0600

    Added starter code and initialized server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,22 @@
 import chess.*;
+import com.google.gson.Gson;
+import spark.Spark;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Spark.port(8080);
+        Spark.staticFiles.location("/web");
+        createRoutes();
+        Spark.awaitInitialization();
+        System.out.println("Listening on port " + 8080);
+
+    }
+
+    private static void createRoutes() {
+        Spark.get("/hello", (req, res) -> "Hello BYU!");
     }
 }
