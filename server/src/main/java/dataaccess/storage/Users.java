5b3bab2a11560876681c001ca840a26ff5fa03e9
Cameron Braddock <cameron.braddock21@gmail.com>
2024-10-17 12:06:10 -0600

    finished updating data access objects and data storage

--- server/src/main/java/dataaccess/storage/Users.java
+++ server/src/main/java/dataaccess/storage/Users.java
@@ -6,4 +6,21 @@ import java.util.ArrayList;
 
 public class Users implements UserDA{
     List<UserData> userList = new ArrayList<UserData>();
+
+    public void clearUsers(){
+        userList = new ArrayList<UserData>();
+    }
+
+    public void createUser(UserData user){
+        userList.add(user);
+    }
+
+    public UserData getUser(String username){
+        for(int i = 0; i < userList.size(); i++){
+            if(userList.get(i).userName().equals(username)){
+                return userList.get(i);
+            }
+        }
+        return null;
+    }
 }
